ecec18adfadd0ee4a585a328054940fc33908e22
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-08 16:15:23 -0400

    completed task1&2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-let mainEl = document.createElement('main')
+let mainEl = document.querySelector('main')
+mainEl.style.backgroundColor = 'var(--main-bg)'
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+mainEl.classList.add('flex-ctr')
+
+let topMenuEl = document.querySelector('nav')
+
+
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+topMenuEl.classList.add('flex-around')
