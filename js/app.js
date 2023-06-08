b045a1f77f23746b91fb4d8501a0cba9f39694b5
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-08 16:40:15 -0400

    completed task 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+const menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
 let mainEl = document.querySelector('main')
 mainEl.style.backgroundColor = 'var(--main-bg)'
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
@@ -9,3 +16,13 @@ let topMenuEl = document.querySelector('nav')
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 topMenuEl.classList.add('flex-around')
+
+ 
+
+
+menuLinks.forEach(link => {
+  let a = document.createElement('a')
+  a.innerText = link.text
+  a.setAttribute('href', link.href)
+  topMenuEl.appendChild(a)
+})
