103159ceebf0f17780f16fcea3f4a2169bf28b5f
Damola Olusakin <olusakinadedamola@gmail.com>
2023-06-09 12:07:02 -0400

    completed task 4

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
 // Menu data structure
 const menuLinks = [
   {text: 'about', href: '/about'},
-  {text: 'catalog', href: '/catalog'},
-  {text: 'orders', href: '/orders'},
-  {text: 'account', href: '/account'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
 ];
 
-let mainEl = document.querySelector('main')
+
+const mainEl = document.querySelector('main')
 mainEl.style.backgroundColor = 'var(--main-bg)'
 mainEl.style.height = '100vh'
 
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
 mainEl.classList.add('flex-ctr')
 
-let topMenuEl = document.querySelector('nav')
+const topMenuEl = document.querySelector('nav')
 
 
 topMenuEl.style.height = "100%";
@@ -29,3 +41,15 @@ menuLinks.forEach(link => {
   a.setAttribute('href', link.href)
   topMenuEl.appendChild(a)
 })
+
+const subMenuEl = document.querySelector('#sub-menu')
+
+subMenuEl.style.height = '100%'
+
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
+
+subMenuEl.classList.add('flex-around')
+
+subMenuEl.style.position = 'absolute'
+
+subMenuEl.style.top = '0'
